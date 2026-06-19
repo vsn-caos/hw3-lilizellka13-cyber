10f0631a482dbad5961fa11389dc4f6e86660d13
lilizellka13-cyber <lilizellka13@gmail.com>
2026-06-19 19:56:43 +0300

    Update solution.c

--- problem4-sockets-tcp-client/solution.c
+++ problem4-sockets-tcp-client/solution.c
@@ -23,11 +23,73 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO: создайте TCP-сокет (AF_INET, SOCK_STREAM),
-    //       заполните struct sockaddr_in с помощью inet_aton/inet_pton,
-    //       подключитесь через connect,
-    //       реализуйте цикл чтения/отправки/приёма/вывода чисел.
-    //       Порядок байт — Little Endian (на x86/x86_64 это нативный порядок).
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(argv[2]));
+
+    if (inet_pton(AF_INET, argv[1], &addr.sin_addr) != 1) {
+        perror("inet_pton");
+        close(sock);
+        return 1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        perror("connect");
+        close(sock);
+        return 1;
+    }
+
+    int value;
+
+    while (scanf("%d", &value) == 1) {
+        int32_t send_value = (int32_t)value;
+
+        ssize_t sent = 0;
+        while (sent < (ssize_t)sizeof(send_value)) {
+            ssize_t res = send(sock,
+                               ((char *)&send_value) + sent,
+                               sizeof(send_value) - sent,
+                               0);
+            if (res <= 0) {
+                close(sock);
+                return 0;
+            }
+            sent += res;
+        }
+
+        int32_t recv_value;
+        ssize_t received = 0;
+
+        while (received < (ssize_t)sizeof(recv_value)) {
+            ssize_t res = recv(sock,
+                               ((char *)&recv_value) + received,
+                               sizeof(recv_value) - received,
+                               0);
+            if (res == 0) {
+                close(sock);
+                return 0;
+            }
+            if (res < 0) {
+                perror("recv");
+                close(sock);
+                return 1;
+            }
+            received += res;
+        }
+
+        printf("%d\n", recv_value);
+        fflush(stdout);
+    }
+
+    close(sock);
 
     return 0;
 }
