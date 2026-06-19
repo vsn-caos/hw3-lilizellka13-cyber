0179ca702afe124adf02805b69577959db885351
lilizellka13-cyber <lilizellka13@gmail.com>
2026-06-19 19:54:31 +0300

    Update solution.c

--- problem3-pipe-connect-2-processes/solution.c
+++ problem3-pipe-connect-2-processes/solution.c
@@ -15,10 +15,56 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO: создайте канал (pipe),
-    //       запустите CMD1 (argv[1]) так, чтобы его stdout → write-конец канала,
-    //       запустите CMD2 (argv[2]) так, чтобы его stdin  ← read-конец канала,
-    //       дождитесь завершения обоих дочерних процессов.
+    int pipefd[2];
+
+    if (pipe(pipefd) < 0) {
+        perror("pipe");
+        return 1;
+    }
+
+    pid_t pid1 = fork();
+
+    if (pid1 < 0) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid1 == 0) {
+        dup2(pipefd[1], STDOUT_FILENO);
+
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        execlp(argv[1], argv[1], NULL);
+
+        perror("execlp");
+        _exit(1);
+    }
+
+    pid_t pid2 = fork();
+
+    if (pid2 < 0) {
+        perror("fork");
+        return 1;
+    }
+
+    if (pid2 == 0) {
+        dup2(pipefd[0], STDIN_FILENO);
+
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        execlp(argv[2], argv[2], NULL);
+
+        perror("execlp");
+        _exit(1);
+    }
+
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
 
     return 0;
 }
